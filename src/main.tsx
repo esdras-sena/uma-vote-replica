14fd7b9d48760800db2e8d0caccb8e7ad7d3afa9
esdras-sena <esantoz@protonmail.com>
2026-01-26 12:08:54 -0300

    adding starknet wallet connect

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { StarknetProvider } from "./provider/starknet-provider";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <StarknetProvider>
+      <App />
+    </StarknetProvider>
   </React.StrictMode>
 );
